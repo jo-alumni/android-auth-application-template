f574a092a8fc505fcb326cf33b842a98aef7e221
nakamurajo <u249101d@alumni.osaka-u.ac.jp>
2026-09-06 18:02:16 +0900

    chore: increase items

--- data/src/main/java/com/example/authapplication/data/item/ItemRepositoryImpl.kt
+++ data/src/main/java/com/example/authapplication/data/item/ItemRepositoryImpl.kt
@@ -11,7 +11,7 @@ import kotlinx.coroutines.flow.flowOf
 @Singleton
 class ItemRepositoryImpl @Inject constructor() : ItemRepository {
 
-    private val mockItems = (1..5).map { index -> Item(id = index.toString(), title = "Item $index") }
+    private val mockItems = (1..30).map { index -> Item(id = index.toString(), title = "Item $index") }
 
     override fun observeItems(): Flow<List<Item>> = flowOf(mockItems)
 
