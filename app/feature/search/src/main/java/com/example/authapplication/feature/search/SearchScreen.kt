e22c059dc701851449e024a29b1e610262713cbf
nakamurajo <u249101d@alumni.osaka-u.ac.jp>
2026-09-08 00:05:33 +0900

    feat: add ime padding

--- app/feature/search/src/main/java/com/example/authapplication/feature/search/SearchScreen.kt
+++ app/feature/search/src/main/java/com/example/authapplication/feature/search/SearchScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -33,7 +34,8 @@ fun SearchScreen(
     Column(
         modifier = modifier
             .fillMaxSize()
-            .padding(horizontal = 16.dp),
+            .padding(horizontal = 16.dp)
+            .imePadding(),
         verticalArrangement = Arrangement.spacedBy(16.dp),
     ) {
         // 検索欄はどの状態でも操作できるよう when の外に置く。
