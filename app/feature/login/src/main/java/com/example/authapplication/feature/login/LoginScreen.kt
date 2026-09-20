044de6843595678fdc82308f8e2ec7929f543e63
nakamurajo <u249101d@alumni.osaka-u.ac.jp>
2026-09-20 18:49:14 +0900

    feat: ログイン画面のキーボードタイプを調整

--- app/feature/login/src/main/java/com/example/authapplication/feature/login/LoginScreen.kt
+++ app/feature/login/src/main/java/com/example/authapplication/feature/login/LoginScreen.kt
@@ -28,6 +28,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.contentType
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -62,7 +63,7 @@ fun LoginScreen(
             onValueChange = { id = it },
             label = { Text(stringResource(R.string.feature_login_id_label)) },
             singleLine = true,
-            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next, keyboardType = KeyboardType.Text),
             keyboardActions = KeyboardActions(
                 onNext = { passwordFocusRequester.requestFocus() },
             ),
@@ -76,7 +77,7 @@ fun LoginScreen(
             label = { Text(stringResource(R.string.feature_login_password_label)) },
             singleLine = true,
             visualTransformation = PasswordVisualTransformation(),
-            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done, keyboardType = KeyboardType.Password),
             keyboardActions = KeyboardActions(
                 onDone = { keyboardController?.hide() },
             ),
